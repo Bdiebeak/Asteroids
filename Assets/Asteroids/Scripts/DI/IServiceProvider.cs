de938e97827106327a2c84c557d5f2e9f65a4047
Bdiebeak <123redcreeper123@gmail.com>
2024-05-24 08:15:24 +0300

    Add base DI

---
+++ Assets/Asteroids/Scripts/DI/IServiceProvider.cs
@@ -0,0 +1,8 @@
+﻿namespace Asteroids.Scripts.DI
+{
+	public interface IServiceProvider
+	{
+		void Bind<TService>(TService implementation);
+		TService Resolve<TService>();
+	}
+}
---
+++ Assets/Asteroids/Scripts/DI/ServiceProvider.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Asteroids.Scripts.DI
+{
+	public class ServiceProvider : IServiceProvider
+	{
+		private Dictionary<Type, object> _services = new();
+
+		public void Bind<TService>(TService implementation)
+		{
+			_services[typeof(TService)] = implementation;
+		}
+
+		public TService Resolve<TService>()
+		{
+			return (TService)_services[typeof(TService)];
+		}
+	}
+}
